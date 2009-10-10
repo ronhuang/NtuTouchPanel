86370c2bbb8e49c40ef9ae4bfdb6d5207f9b5b99
Ron Huang <ronhuang@gmail.com>
2009-10-10 15:37:19 +0800

    Moved the value mapping function for all panels to TpValue.cpp.

--- src/TpValue.cpp
+++ src/TpValue.cpp
@@ -15,7 +15,7 @@ void tpValueInit()
   }
 }
 
-//
+//------------------------------------------------------------------------------
 //
 void tpValueCalculateAverage(long valueA, long valueB, long &aveA, long &aveB)
 {
@@ -26,41 +26,57 @@ void tpValueCalculateAverage(long valueA, long valueB, long &aveA, long &aveB)
   initialValueA[0] = valueA;
   initialValueB[0] = valueB;
 
-  aveA = (initialValueA[0] + initialValueA[1] + initialValueA[2] + initialValueA[3]) / 4;
-  aveB = (initialValueB[0] + initialValueB[1] + initialValueB[2] + initialValueB[3]) / 4;
+  aveA = (initialValueA[0] + initialValueA[1] +
+          initialValueA[2] + initialValueA[3]) / 4;
+  aveB = (initialValueB[0] + initialValueB[1] +
+          initialValueB[2] + initialValueB[3]) / 4;
 }
 
-//
+//------------------------------------------------------------------------------
 //
 long tpValueMappingA(long value)
 {
 #if TP_PANEL_NUMBER == 1
+  return map(value, 0XFL, 16000000L, 0, 255);
 #elif TP_PANEL_NUMBER == 2
+  return map(value, 0XFL, 16000000L, 0, 300);
 #elif TP_PANEL_NUMBER == 3
+  return map(value, 0XFL, 16000000L, 0, 300);
 #elif TP_PANEL_NUMBER == 4
   return map(value, 0XFL, 16000000L, 0, 300);
 #elif TP_PANEL_NUMBER == 5
+  return map(value, 0XFL, 16000000L, 0, 300);
 #elif TP_PANEL_NUMBER == 6
+  return map(value, 0XFL, 16000000L, 0, 300);
 #elif TP_PANEL_NUMBER == 7
+  return map(value, 0XFL, 16000000L, 0, 300);
 #elif TP_PANEL_NUMBER == 8
+  return map(value, 0XFL, 16000000L, 0, 255);
 #else
 #error TP_PANEL_NUMBER invalid.
 #endif
 }
 
-//
+//------------------------------------------------------------------------------
 //
 long tpValueMappingB(long value)
 {
 #if TP_PANEL_NUMBER == 1
+  return map(value, 0XFL, 16000000L, 0, 300);
 #elif TP_PANEL_NUMBER == 2
+  return map(value, 0XFL, 16000000L, 0, 300);
 #elif TP_PANEL_NUMBER == 3
+  return map(value, 0XFL, 16000000L, 0, 300);
 #elif TP_PANEL_NUMBER == 4
   return map(value, 0XFL, 16000000L, 0, 300);
 #elif TP_PANEL_NUMBER == 5
+  return map(value, 0XFL, 16000000L, 0, 300);
 #elif TP_PANEL_NUMBER == 6
+  return map(value, 0XFL, 16000000L, 0, 300);
 #elif TP_PANEL_NUMBER == 7
+  return map(value, 0XFL, 16000000L, 0, 300);
 #elif TP_PANEL_NUMBER == 8
+  return -map(value, 0, 17000000L, 0, 255);
 #else
 #error TP_PANEL_NUMBER invalid.
 #endif
